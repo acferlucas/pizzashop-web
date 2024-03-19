36bdb5ee2f68d6e443a948cc9f7dce0fc7df2787
acferlucas <acferlucas.dev@gmail.com>
2024-03-19 15:22:07 -0300

    pagina:login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './global.css'
 
-import { Button } from '@/components/ui/button'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <Button>Enviar</Button>
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
+export function DashBoard() {
+  return (
+    <>
+      <Helmet title="dashboard" />
+      <h1>dashboard</h1>
+    </>
+  )
+}
